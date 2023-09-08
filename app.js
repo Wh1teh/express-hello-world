6780a4a1b825330fe42852adb0732b22da5fee08
Miika Tiihonen <m95.tiihonen@gmail.com>
2023-09-08 22:41:57 +0300

    asd

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'html/cube.html'));
-});
+app.use(express.static(__dirname + '/public'));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
